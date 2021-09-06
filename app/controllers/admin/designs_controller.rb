b09a14a8d54272d19089f0c39111f4dee74d4c5a
shoponcom001 <shopon.com001@gmail.com>
2021-09-06 09:25:18 +0000

    [Add]デザイン一覧

--- app/controllers/admin/designs_controller.rb
+++ app/controllers/admin/designs_controller.rb
@@ -1,8 +1,10 @@
 class Admin::DesignsController < ApplicationController
   def index
+    @designs = Design.all
   end
 
   def new
+    @design = Design.new
   end
 
   def create
--- app/models/design.rb
+++ app/models/design.rb
@@ -1,3 +1,6 @@
 class Design < ApplicationRecord
   has_many :items
+  attachment :image
+
+  enum is_deleted: { on_sale: false, stop_sale: true}
 end
