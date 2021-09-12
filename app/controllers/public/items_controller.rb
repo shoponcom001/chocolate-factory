65a05d3b39a4fdc713415f14d39e109e8fd9ef0e
shoponcom001 <shopon.com001@gmail.com>
2021-09-12 13:08:59 +0000

    [Update]注文商品実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,6 +8,15 @@ class Public::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    rank = Rank.find(params[:rank_id])
+    color = Color.find(params[:color_id])
+    last = Last.find(params[:last_id])
+    design = Design.find(params[:item][:design_id])
+    @item.buy_name = rank.name + color.name + last.name
+    @item.buy_price = rank.price + design.price
+    if @item.material == "buy"
+      @item.buy_price += 5000
+    end
     @item.save
     redirect_to new_order_path
   end
@@ -15,7 +24,7 @@ class Public::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:buy_name, :material, :size, :buy_price)
+    params.require(:item).permit(:buy_name, :material, :size, :buy_price, :design_id, :period_id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :design
-  belongs_to :period_id
+  belongs_to :period
 
   enum material: { buy: true, not_buy: false }
 
