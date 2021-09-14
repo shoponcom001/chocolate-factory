0dec44b7b919e4f46a46056ae0743a71130b683c
shoponcom001 <shopon.com001@gmail.com>
2021-09-14 11:51:58 +0000

    [Add]注文情報実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :order, optional: true
+  # has_one :order
   belongs_to :design
   belongs_to :period
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_one :item
+  # belongs_to :item
   
   enum payment_method: { credit_card: 0, bank_transfer: 1 }
   
