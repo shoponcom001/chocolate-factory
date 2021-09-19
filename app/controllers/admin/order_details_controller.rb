42175ebcd82bb87cde1b2ec73d40a475a1d8bad2
shoponcom001 <shopon.com001@gmail.com>
2021-09-19 12:58:42 +0000

    [Add]管理者側注文詳細実装

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,25 @@
 class Admin::OrderDetailsController < Admin::AdminsController
-  
+
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    @order = @item.order
+    @item = @order.item
+    if @item.production_status == "during_production"
+      @order.order_status = "in_production"
+      @order.save
+      ContactMailer.production_mail(@order).deliver
+    elsif @item.production_status == "production_completed"
+      @order.order_status = "preparing_delivery"
+      @order.save
+    end
+    redirect_to request.referer
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:production_status)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,24 @@
 class Admin::OrdersController < Admin::AdminsController
+
   def show
+    @order = Order.find(params[:id])
+    @item = @order.item
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    if @order.order_status == "delivered"
+      ContactMailer.completed_mail(@order).deliver
+    end
+    redirect_to request.referer
+
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
+
 end
