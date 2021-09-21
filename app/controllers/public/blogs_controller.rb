4f0ed825745eee2c6b74246258a600377617c646
shoponcom001 <shopon.com001@gmail.com>
2021-09-21 12:00:47 +0000

    [update]修正

--- app/controllers/public/blogs_controller.rb
+++ app/controllers/public/blogs_controller.rb
@@ -1,7 +1,9 @@
 class Public::BlogsController < ApplicationController
   def index
+    @blogs = Blog.all.order(created_at: :desc)
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 end
