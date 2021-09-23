671ffca61e4026e40e6cc48aba9e75863ae20757
shoponcom001 <shopon.com001@gmail.com>
2021-09-23 08:58:07 +0000

    [Add]非同期

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -4,14 +4,12 @@ class Public::LikesController < ApplicationController
     @blog = Blog.find(params[:blog_id])
     @like = @blog.likes.new(user_id: current_user.id)
     @like.save
-    redirect_to request.referer
   end
 
   def destroy
     @blog = Blog.find(params[:blog_id])
     @like = @blog.likes.find_by(user_id: current_user.id)
     @like.destroy
-    redirect_to request.referer
   end
 
 end
