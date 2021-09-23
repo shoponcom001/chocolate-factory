532c89f97a767c6ff765b794315ee2b3a1a6b4b9
shoponcom001 <shopon.com001@gmail.com>
2021-09-23 02:28:44 +0000

    [Add]いいね、コメント実装

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,9 +1,17 @@
 class Public::LikesController < ApplicationController
-  
+
   def create
+    @blog = Blog.find(params[:blog_id])
+    @like = @blog.likes.new(user_id: current_user.id)
+    @like.save
+    redirect_to request.referer
   end
-  
+
   def destroy
+    @blog = Blog.find(params[:blog_id])
+    @like = @blog.likes.find_by(user_id: current_user.id)
+    @like.destroy
+    redirect_to request.referer
   end
-  
+
 end
