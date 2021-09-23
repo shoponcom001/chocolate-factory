ff3ff52bc2170ad45966056e80409080d100654c
shoponcom001 <shopon.com001@gmail.com>
2021-09-23 08:58:07 +0000

    [Add]非同期

--- app/controllers/public/blog_comments_controller.rb
+++ app/controllers/public/blog_comments_controller.rb
@@ -5,20 +5,13 @@ class Public::BlogCommentsController < ApplicationController
     @blog_comment = BlogComment.new(blog_comment_params)
     @blog_comment.blog_id = @blog.id
     @blog_comment.user_id = current_user.id
-    if  @blog_comment.save
-      redirect_to blog_path(@blog.id)
-    else
-      flash[:alert] = "コメントを入力してください"
-      redirect_to request.referer
-    end
+    @blog_comment.save
   end
 
   def destroy
     @blog = Blog.find(params[:blog_id])
     blog_comment = @blog.blog_comments.find(params[:id])
     blog_comment.destroy
-    flash[:alert] = "削除しました"
-    redirect_to request.referer
   end
 
   private
