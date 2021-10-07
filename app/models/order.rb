1c539057f6608b77e37983e28d818132453fc61c
shoponcom001 <shopon.com001@gmail.com>
2021-10-07 08:16:37 +0000

    [Add]住所自動入力

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,10 +15,4 @@ class Order < ApplicationRecord
     delivered: 4
   }
 
-  # validates :postal_code, presence: true, format: { with: /\A\d{7}\z/, message: '「-」なしの半角数字で入力してください' }
-  # validates :address, presence: true
-  # validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください'}
-  # validates :phone_number, format: { with: /\A\d{10,11}\z/}
-  # validates :payment_method, presence: true
-
 end
